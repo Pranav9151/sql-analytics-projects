6402bf290de51b283d07644893dc6b43572eff83
pranav9151 <pranavmahajan9151@gmail.com>
2026-01-13 16:41:48 +0530

    Day 2: Customer segmentation and conversion analysis

--- queries/02_joins_analysis.sql
+++ queries/02_joins_analysis.sql
@@ -67,3 +67,37 @@ ORDER BY conversion_rate DESC;
 
 
 
+
+
+-- ======================================
+-- 3. Conversion Rate by Balance Segment
+-- ======================================
+
+WITH customer_responses AS (
+    SELECT
+        c.customer_id,
+        c.age,
+        c.job,
+        c.balance,
+        r.response
+    FROM customers c
+    JOIN responses r
+        ON c.customer_id = r.customer_id
+)
+
+
+SELECT
+    CASE
+        WHEN balance < 500 THEN 'Low Balance'
+        WHEN balance BETWEEN 500 AND 2000 THEN 'Mid Balance'
+        ELSE 'High Balance'
+    END AS balance_segment,
+    COUNT(*) AS total_customers,
+    COUNT(CASE WHEN response = 'yes' THEN 1 END) AS total_converted,
+    ROUND(
+        COUNT(CASE WHEN response = 'yes' THEN 1 END) * 100.0 / COUNT(*),
+        2
+    ) AS conversion_rate
+FROM customer_responses
+GROUP BY balance_segment
+ORDER BY conversion_rate DESC;
